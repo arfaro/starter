12d5979ee607725e1791dd022ea351e34b806ca1
arfaro <cikasarung@gmail.com>
2021-12-04 10:07:17 +0700

    "Tambah Font"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 export default function IndexPage() {
   return (
-    <div className="text-yellow-70">Hello, Travelo!</div>
+    <>
+    <div className="text-yellow-70 text-label text-2xl font-bold">Hello, Travelo!</div>
+    <div className="text-yellow-70 text-2xl font-sans">Hello, Travelo!</div>
+    </>
   )
 }
